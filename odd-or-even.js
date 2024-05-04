f4945d580d2e62ec5bd70a06db68ea2864ca0569
lily <lilyjulymm2009@gmail.com>
2024-05-04 21:19:43 +0630

    02-odd-or-even

---
+++ odd-or-even.js
@@ -0,0 +1,15 @@
+var oddOrEven = function(num){
+  if (num % 2 == 0){
+    console.log("Even");
+  } else{
+    console.log("odd");
+  };
+}
+
+var oddOrEven = function(num){
+  if(num % 2 === 0){
+    return("even");
+  } else{
+    return("odd");
+  };
+}
